c3f6afbf9a690ffaa33d111446a26c4223817553
Tornike Parunashvili <parunashvilitornike@gmail.com>
2025-03-01 14:46:58 +0400

    Initial commit

---
+++ ReadlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadlyApp.swift
+//  Readly
+//
+//  Created by tornike <parunashvili on 01.03.25.
+//
+
+import SwiftUI
+
+@main
+struct ReadlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
